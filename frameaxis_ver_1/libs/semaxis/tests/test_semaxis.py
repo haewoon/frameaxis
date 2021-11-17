7e7ae789e26081e9a2c272f97dfd6d5a6ae06352
btruong <27907677+btrantruong@users.noreply.github.com>
2021-11-17 16:38:00 -0500

    Unzip folder

--- frameaxis_ver_1/libs/semaxis/tests/test_semaxis.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(1, '..')
-from semaxis import CoreUtil
-from semaxis import SemAxis
-
-def test_load_google_embedding():
-    sa = SemAxis(CoreUtil.load_embedding("../../../data/embeddings/GoogleNews-vectors-negative300.bin"))
-    assert sa.embedding_vector_size() == 300
